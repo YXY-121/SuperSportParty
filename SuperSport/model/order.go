12935c6c8d0aa92896fe84f95b0db35df96b632b
yanxy <835264630@qq.com>
2022-02-09 22:41:01 +0800

    开始重构项目

--- SuperSport/model/order.go
+++ SuperSport/model/order.go
@@ -3,19 +3,20 @@ package model
 import "time"
 
 type Order struct {
-	OrderId string `gorm:"order_id"`
-	CreaterId string `gorm:"creater_id"`
-	SportType string `gorm:"sport_type"`
-	Description string `gorm:"description"`
-	TotalNumber int `gorm:"total_number"`
-	RestNumber int `gorm:"rest_number"`
-	OrderLocation string`gorm:"order_location"`
-	Longitude string `gorm:"longitude"`
-	Latitude string `gorm:"latitude"`
-	CreateTime time.Time `gorm:"create_time"`
-	EndTime time.Time `gorm:"end_time"`
-
+	OrderId       string    `gorm:"order_id",json:"order_id"`
+	CreaterId     string    `gorm:"creater_id,json:"creater_id"`
+	SportType     string    `gorm:"sport_type",json:"sport_type"`
+	Description   string    `gorm:"description",json:"description"`
+	TotalNumber   int       `gorm:"total_number",json:"total_number"`
+	RestNumber    int       `gorm:"rest_number",json:"rest_number"`
+	OrderLocation string    `gorm:"order_location",json:"order_location"`
+	Longitude     string    `gorm:"longitude",json:"longitude"`
+	Latitude      string    `gorm:"latitude",json:"latitude"`
+	CreateTime    time.Time `gorm:"create_time",json:"create_time"`
+	EndTime       time.Time `gorm:"end_time"`
+	StringEndTime string    `json:"endTime"`
 }
-func  (Order)TableName() string {
+
+func (Order) TableName() string {
 	return "order"
 }
--- SuperSport/model/user.go
+++ SuperSport/model/user.go
@@ -1,8 +1,14 @@
 package model
+
 type User struct {
-	UserId string `gorm:"user_id"`
-}
-func  (User)TableName() string {
-	return "user"
+	UserId          string `gorm:"user_id"`
+	UserName        string `gorm:"user_name"`
+	City            string `gorm:"city"`
+	ChatHead        string `gorm:"chat_head"`
+	SelfDescription string `gorm:"self_description"`
+	IsLock          int    `gorm:"is_lock"`
 }
 
+func (User) TableName() string {
+	return "user"
+}
